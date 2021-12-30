53973cca1574288d4a4c478a1ce6f6d24ed2d73e
Christoffer Green <christoffer.green@gmail.com>
2021-12-30 14:40:33 +0100

    Linking test now passes

--- src/PathSpace.hpp
+++ src/PathSpace.hpp
@@ -8,11 +8,12 @@
 
 namespace Forsoning {
 
-struct Security {
-    bool insert = false;
-    bool grab   = false;
+enum struct SecurityType {
+    Grab = 0,
+    Insert
 };
-using SecFunT = std::function<Security(std::filesystem::path const &path)>;
+using SecFunT = std::function<bool(std::filesystem::path const &path, SecurityType const &sec)>;
+inline const SecFunT SecurityAlwaysAllow = [](std::filesystem::path const &path, SecurityType const &sec){ return true; };
 
 struct Data {
     Data() = default;
@@ -28,69 +29,86 @@ struct Data {
     std::variant<int> var;
 };
 
-struct PathSpaceTE {
-	PathSpaceTE() = default;
-	template<typename T>
-	PathSpaceTE(T x)                    : self(std::make_unique<model<T>>(std::move(x))) {}
-	PathSpaceTE(PathSpaceTE const &rhs) : self(rhs.self->copy_())                        {}
-
-	PathSpaceTE& operator=(PathSpaceTE const &rhs) {return *this = PathSpaceTE(rhs);}
-	PathSpaceTE& operator=(PathSpaceTE&&) noexcept = default;
-
-	auto insert(std::filesystem::path const &path, Data const &data) -> void                { this->self->insert_(path, data); }
-	auto grab(std::filesystem::path const &path)                     -> std::optional<Data> { return this->self->grab_(path); }
-private:
+class PathSpaceTE {
 	struct concept_t {
 		virtual ~concept_t() = default;
 		
-		virtual auto copy_()                                                      const -> std::unique_ptr<concept_t> = 0;
+		virtual auto copy_()                                                      const -> std::shared_ptr<concept_t> = 0;
+		virtual auto size_()                                                      const -> int                       = 0;
 		virtual auto insert_(std::filesystem::path const &path, Data const &data)       -> void                       = 0;
 		virtual auto grab_(std::filesystem::path const &path)                           -> std::optional<Data>        = 0;
 	};
+public:
+	PathSpaceTE() = default;
+	template<typename T>
+	PathSpaceTE(T x)                             : self(std::make_shared<model<T>>(std::move(x))) {}
+	PathSpaceTE(PathSpaceTE const &rhs)          : self(rhs.self->copy_())                        {}
+	PathSpaceTE(std::shared_ptr<concept_t> self) : self(self)                                     {}
+
+	auto operator=(PathSpaceTE const &rhs) -> PathSpaceTE& {return *this = PathSpaceTE(rhs);}
+	auto operator=(PathSpaceTE&&) noexcept -> PathSpaceTE& = default;
+
+	auto link()                                                      const -> PathSpaceTE         { return this->self; }
+	auto size()                                                      const -> int                 { return this->self->size_(); }
+	auto insert(std::filesystem::path const &path, Data const &data)       -> void                { this->self->insert_(path, data); }
+	auto grab(std::filesystem::path const &path)                           -> std::optional<Data> { return this->self->grab_(path); }
+private:
 	template<typename T> 
 	struct model final : concept_t {
 		model(T x) : data(std::move(x)) {}
 
-		auto copy_() const -> std::unique_ptr<concept_t> override {return std::make_unique<model>(*this);}
-		
-		auto insert_(std::filesystem::path const &path, Data const &d)       -> void                override {return this->data.insert(path, d);}
-		auto grab_(std::filesystem::path const &path)                        -> std::optional<Data> override {return this->data.grab(path);}
+		auto copy_()                                                   const -> std::shared_ptr<concept_t> override {return std::make_shared<model>(*this);}
+		auto size_()                                                   const -> int                        override {return this->data.size();}
+		auto insert_(std::filesystem::path const &path, Data const &d)       -> void                       override {return this->data.insert(path, d);}
+		auto grab_(std::filesystem::path const &path)                        -> std::optional<Data>        override {return this->data.grab(path);}
 		
 		T data;
 	};
-	std::unique_ptr<concept_t> self;
+	std::shared_ptr<concept_t> self;
 };
 
 struct PathSpace {
-    protected:
+protected:
     friend PathSpaceTE;
+
     auto insert(std::filesystem::path const &path, Data const &data) -> void {
         this->data.insert(std::make_pair(path.filename().string(),  data));
     };
+
     auto grab(std::filesystem::path const &path) -> std::optional<Data> {
         std::string const name = path.filename().string();
         if(this->data.count(name))
             return this->data.extract(name).mapped();
         return {};
     };
+
+    auto size() const -> int {
+        return this->data.size();
+    }
+
     std::unordered_multimap<std::string, Data> data;
 };
 
 struct View {
-    View(PathSpaceTE space, SecFunT const &security) : space(space), security(security) {};
+    View(PathSpaceTE const &space, SecFunT const &security) : space(space.link()), security(security) {};
     
     auto insert(std::filesystem::path const &path, Data const &data) -> void {
-        if(this->security(path).insert)
+        if(this->security(path, SecurityType::Insert))
             this->space.insert(path, data);
     };
+
     template<typename T>
     auto grab(std::filesystem::path const &path) -> std::optional<T> { 
-        if(this->security(path).grab) {
+        if(this->security(path, SecurityType::Grab)) {
             if(auto const val = this->space.grab(path))
                 return val.value().to<T>();
         }
         return {};
     };
+
+    auto size() const -> int {
+        return this->space.size();
+    }
 
     private:
         PathSpaceTE space;
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,11 +3,44 @@
 
 #include "PathSpace.hpp"
 
-TEST_CASE("Standard") {
-    Forsoning::PathSpace space;
-    Forsoning::View view(space, [](std::filesystem::path const &path){return Forsoning::Security{true, true};});
+using namespace Forsoning;
+
+TEST_CASE("Insert And Grab") {
+    View view(PathSpace{}, SecurityAlwaysAllow);
     view.insert("/test", 5);
-    auto const res = view.grab<int>("/test");
+    view.insert("/test", 4);
+    view.insert("/test", 3);
+    auto res = view.grab<int>("/test");
     CHECK(res.has_value());
     CHECK(*res == 5);
+    res = view.grab<int>("/test");
+    CHECK(res.has_value());
+    CHECK(*res == 4);
+    res = view.grab<int>("/test");
+    CHECK(res.has_value());
+    CHECK(*res == 3);
+    res = view.grab<int>("/test");
+    CHECK(!res.has_value());
+}
+
+TEST_CASE("Space Linking") {
+    PathSpaceTE space(PathSpace{});
+    View view(space, SecurityAlwaysAllow);
+    view.insert("/test", 5);
+    CHECK(space.size() == view.size());
+    View viewCopy(PathSpaceTE{space}, SecurityAlwaysAllow);
+    CHECK(viewCopy.size() == 1);
+    View viewLink(space, SecurityAlwaysAllow);
+    CHECK(viewLink.size() == 1);
+
+    auto res = view.grab<int>("/test");
+    CHECK(res.has_value());
+    CHECK(*res == 5);
+
+    auto resCopy = viewCopy.grab<int>("/test");
+    CHECK(resCopy.has_value());
+    CHECK(*resCopy == 5);
+
+    auto resLink = viewLink.grab<int>("/test");
+    CHECK(!resLink.has_value());
 }
