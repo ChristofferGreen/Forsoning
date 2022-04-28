2afa63f4b1e023c0bacc99f6fefd81a525239dd5
Christoffer Green <christoffer.green@gmail.com>
2022-04-28 10:26:56 +0200

    Refac

--- src/FSNG/CodicesAegis.hpp
+++ src/FSNG/CodicesAegis.hpp
@@ -9,14 +9,6 @@ struct CodicesAegis {
     CodicesAegis() = default;
     CodicesAegis(CodicesAegis const &other) : codices(other.codices) {}
 
-    auto count(auto const &str) const -> int{
-        return this->codices.count(str);
-    }
-
-    auto push_back(auto const &name, Data const &data) {
-        this->codices[name].insert(data);
-    }
-
     template<typename T>
     auto visitFirst(auto const &name, auto const &fun) {
         if(this->codices.contains(name)) {
@@ -25,15 +17,25 @@ struct CodicesAegis {
         return false;
     }
 
-    auto readMutex() const -> std::shared_lock<std::shared_mutex> {
-        return std::shared_lock<std::shared_mutex>(this->mutex);
+    auto writeCodices(auto const &fun) -> void {
+        auto const mapWriteMutex = this->writeMutex();
+        fun(this->codices);
     }
 
-    auto writeMutex() const -> std::lock_guard<std::shared_mutex> {
-        return std::lock_guard<std::shared_mutex>(this->mutex);
+    auto readCodices(auto const &fun) const -> void {
+        auto const mapReadMutex = this->readMutex();
+        fun(this->codices);
     }
+    private:
+        auto readMutex() const -> std::shared_lock<std::shared_mutex> {
+            return std::shared_lock<std::shared_mutex>(this->mutex);
+        }
 
-    std::unordered_map<std::string, Codex> codices;
-    mutable std::shared_mutex mutex;
+        auto writeMutex() const -> std::lock_guard<std::shared_mutex> {
+            return std::lock_guard<std::shared_mutex>(this->mutex);
+        }
+
+        std::unordered_map<std::string, Codex> codices;
+        mutable std::shared_mutex mutex;
 };
 }
--- src/PathSpace.hpp
+++ src/PathSpace.hpp
@@ -27,9 +27,10 @@ struct PathSpace {
         if(range.isAtData())
             return this->insert(range.dataName(), data);
         if(auto const spaceName = range.spaceName()) { // Create space if it does not exist
-            auto const mapWriteMutex = this->codices.writeMutex();
-            if(this->codices.count(spaceName.value())==0)
-                this->codices.push_back(spaceName.value(), PathSpaceTE(PathSpace{this->processor}));
+            this->codices.writeCodices([this, &spaceName](auto &codices){
+                if(codices.count(spaceName.value())==0)
+                    codices[spaceName.value()].insert(PathSpaceTE(PathSpace{this->processor}));
+            });
             return this->codices.visitFirst<PathSpaceTE>(spaceName.value(), [&range, &data](auto &space){return space.insert(range.next(), data);});
         }
         return false;
@@ -41,17 +42,19 @@ struct PathSpace {
 
     virtual auto toJSON() const -> nlohmann::json {
         nlohmann::json json;
-        auto const mapReadMutex = this->codices.readMutex();
-        for(auto const &p : this->codices.codices)
-            json[p.first] = p.second.toJSON();
+        this->codices.readCodices([&json](auto const &codices){
+            for(auto const &p : codices)
+                json[p.first] = p.second.toJSON();
+        });
         return json;
     }
 
 private:
     virtual auto insert(std::string const &dataName, Data const &data) -> bool {
         if(data.isDirectlyInsertable()) {
-            auto const writeMutex = this->codices.writeMutex();
-            this->codices.push_back(dataName, data);
+            this->codices.writeCodices([&dataName, &data](auto &codices){
+                codices[dataName].insert(data);
+            });
         }
         else if(data.is<std::unique_ptr<std::function<Coroutine()>>>())
             return this->insert(dataName, *data.as<std::unique_ptr<std::function<Coroutine()>>>());
