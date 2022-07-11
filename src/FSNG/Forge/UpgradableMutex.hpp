bcfefcad68ba15a26e63c88f034986ee399e527e
Christoffer Green <christoffer.green@gmail.com>
2022-07-11 16:35:01 +0200

    Better u

--- src/FSNG/Forge/UpgradableMutex.hpp
+++ src/FSNG/Forge/UpgradableMutex.hpp
@@ -7,7 +7,6 @@ namespace FSNG {
 struct UpgradableMutex {
     auto lock_shared() -> void {
         this->shared.lock_shared();
-        this->state = 1;
     }
 
     auto upgrade() -> void {
@@ -15,8 +14,6 @@ struct UpgradableMutex {
         this->shared.unlock_shared();
         this->shared.lock(); // Now the only shared mode and upgrading thread
         this->unique.lock(); // Now we have write rights
-        this->state = 2;
-        this->upgraded = true;
     }
 
     auto downgrade() -> void {
@@ -24,21 +21,16 @@ struct UpgradableMutex {
         this->upgrading.unlock();
         this->shared.unlock();
         this->shared.lock_shared();
-        this->state = 1;
-        this->upgraded = false;
     }
 
     auto unlock_shared() -> void {
         this->shared.unlock_shared();
-        this->state = 0;
     }
 
 private:
     mutable std::shared_mutex shared;
     mutable std::mutex upgrading;
     mutable std::mutex unique;
-    int state = 0;
-    bool upgraded = false;
 };
 
 struct Upgrade {
