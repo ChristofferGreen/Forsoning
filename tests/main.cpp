00792bb8bb368edff505b6a3bf7118d25ca5e411
Christoffer Green <christoffer.green@gmail.com>
2022-08-03 11:10:37 +0200

    blah

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,9 +14,5 @@ int main(int argc, char** argv) {
     }
     
     doctest::Context context(argc, argv);
-    int res = context.run();
-    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;          // propagate the result of the tests
-
-    return res;
+    return context.run();
 }
