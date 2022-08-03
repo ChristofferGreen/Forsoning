639196e4e45567d0f86c4fadc49474641e72e55e
Christoffer Green <christoffer.green@gmail.com>
2022-08-03 11:08:32 +0200

    blah

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,30 +6,19 @@
 
 #include "backward.hpp"
 
-#include <chrono>
-#include <thread>
-
-namespace backward {
-backward::SignalHandling sh;
-}
-
 int main(int argc, char** argv) {
     try {
         auto logger = spdlog::basic_logger_mt("file", "logs/basic-log.txt", true);
         logger->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+        spdlog::get("file")->info("main");
     } catch (const spdlog::spdlog_ex &ex) {
         std::cout << "Log init failed: " << ex.what() << std::endl;
     }
-    spdlog::get("file")->info("main");
-    using namespace std::chrono_literals;
     
     doctest::Context context(argc, argv);
     int res = context.run();
-
     if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
         return res;          // propagate the result of the tests
-
-    // client code goes here
 
     return res;
 }
