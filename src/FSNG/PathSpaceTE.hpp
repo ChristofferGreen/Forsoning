3373ff2511c37b0feee6b3fd958deb1a3d8c92a6
Christoffer Green <christoffer.green@gmail.com>
2023-01-26 12:37:43 +0100

    blah

--- src/FSNG/PathSpaceTE.hpp
+++ src/FSNG/PathSpaceTE.hpp
@@ -1,29 +1,24 @@
 #pragma once
-#include <filesystem>
-#include <map>
-#include <condition_variable>
-#include <shared_mutex>
+#include <memory>
 
-#include "FSNG/Coroutine.hpp"
 #include "FSNG/Path.hpp"
-#include "FSNG/Forge/Ticket.hpp"
-#include "FSNG/utils.hpp"
+#include "FSNG/Data.hpp"
 
 #include "nlohmann/json.hpp"
 
 namespace FSNG {
 class PathSpaceTE {
 	struct concept_t {
-		virtual      ~concept_t   ()                                                         = default;
-		virtual auto operator==   (const concept_t &rhs) const -> bool                       = 0;
-		virtual auto copy_        ()                     const -> std::unique_ptr<concept_t> = 0;
+		virtual      ~concept_t()                                                                                                                  = default;
+		virtual auto operator==(const concept_t &rhs)                                                         const  -> bool                       = 0;
+		virtual auto copy_     ()                                                                             const  -> std::unique_ptr<concept_t> = 0;
 		
-		virtual auto toJSON_         ()                                                                             const  -> nlohmann::json             = 0;
-		virtual auto insert_         (Path const &range, Data const &data, Path const &coroResultPath)                     -> bool                       = 0;
-		virtual auto grab_           (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
-		virtual auto grabBlock_      (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
-		virtual auto read_           (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
-		virtual auto readBlock_      (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
+		virtual auto toJSON_   ()                                                                             const  -> nlohmann::json             = 0;
+		virtual auto insert_   (Path const &range, Data const &data,           Path const &coroResultPath)           -> bool                       = 0;
+		virtual auto grab_     (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
+		virtual auto grabBlock_(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
+		virtual auto read_     (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
+		virtual auto readBlock_(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                       = 0;
 	};
 public:
 	template<typename T>
@@ -40,12 +35,12 @@ public:
 	PathSpaceTE(PathSpaceTE &&rhs)               : self(std::move(rhs.self))                      {this->insert("", this);}
 	PathSpaceTE(std::unique_ptr<concept_t> self) : self(std::move(self))                          {this->insert("", this);}
 
-	auto operator= (PathSpaceTE const &rhs)       -> PathSpaceTE& {return *this = PathSpaceTE(rhs);}
-	auto operator= (PathSpaceTE &&rhs) noexcept   -> PathSpaceTE& {
+	auto operator=(PathSpaceTE const &rhs)       -> PathSpaceTE& {return *this = PathSpaceTE(rhs);}
+	auto operator=(PathSpaceTE &&rhs) noexcept   -> PathSpaceTE& {
 		this->self = std::move(rhs.self);
 		return *this;
 	};
-	auto operator==(const PathSpaceTE &rhs) const -> bool         {
+	auto operator==(const PathSpaceTE &rhs) const -> bool        {
 		if(!this->self && !rhs.self)
 			return true;
 		if(!this->self || !rhs.self)
@@ -53,18 +48,14 @@ public:
 		return *this->self == *rhs.self;
 	};
 
-	auto toJSON()                                                                            const -> nlohmann::json      { return this->self->toJSON_(); }
-	auto insert(Path const &range, Data const &data, Path const &coroResultPath="")                -> bool                { 
-		return this->self->insert_(range, data, coroResultPath);
-	}
-    auto grabBlock(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           {
-		return this->self->grabBlock_(range, info, data, isTriviallyCopyable);
-	}
-    auto grab(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool                {
-		return this->self->grab_(range, info, data, isTriviallyCopyable);
-	}
-	template<typename T>
-	auto grab(Path const &range)                                         -> std::optional<T> {
+	auto toJSON()                                                                            const      -> nlohmann::json { return this->self->toJSON_(); }
+	auto insert(Path const &range, Data const &data, Path const &coroResultPath="")                     -> bool           { return this->self->insert_(range, data, coroResultPath); }
+    auto grabBlock(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           { return this->self->grabBlock_(range, info, data, isTriviallyCopyable); }
+    auto grab(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable)      -> bool           { return this->self->grab_(range, info, data, isTriviallyCopyable); }
+    auto read(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable)      -> bool           { return this->self->read_(range, info, data, isTriviallyCopyable); }
+    auto readBlock(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           { return this->self->readBlock_(range, info, data, isTriviallyCopyable); }
+	template<typename T>     
+	auto grab(Path const &range) -> std::optional<T> {
 		T data;
 		if(this->grab(range, &typeid(T), reinterpret_cast<void*>(&data), std::is_trivially_copyable<T>()))
 			return data;
@@ -76,21 +67,15 @@ public:
 		this->grabBlock(range, &typeid(T), reinterpret_cast<void*>(&data), std::is_trivially_copyable<T>());
 		return data;
 	}
-    auto read(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool {
-		return this->self->read_(range, info, data, isTriviallyCopyable);
-	}
 	template<typename T>
-	auto read(Path const &range)                                         -> std::optional<T> {
+	auto read(Path const &range) -> std::optional<T> {
 		T data;
 		if(this->read(range, &typeid(T), reinterpret_cast<void*>(&data), std::is_trivially_copyable<T>()))
 			return data;
 		return std::nullopt;
 	}
-    auto readBlock(Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool {
-		return this->self->readBlock_(range, info, data, isTriviallyCopyable);
-	}
 	template<typename T>
-	auto readBlock(Path const &range)                                         -> T {
+	auto readBlock(Path const &range) -> T {
 		T data;
 		this->readBlock(range, &typeid(T), reinterpret_cast<void*>(&data), std::is_trivially_copyable<T>());
 		return data;
@@ -100,11 +85,11 @@ private:
 	struct model final : concept_t {
 		model() = default;
 		model(T x) : data(std::move(x)) {}
-		auto operator==(const concept_t &rhs) const -> bool override { return this->data==reinterpret_cast<model<T> const&>(rhs).data; }
+		auto operator==(const concept_t &rhs)                                                                const -> bool override { return this->data==reinterpret_cast<model<T> const&>(rhs).data; }
 		auto copy_()                                                                                         const -> std::unique_ptr<concept_t> override {return std::make_unique<model>(*this);}
 		
 		auto toJSON_         ()                                                                              const -> nlohmann::json override {return this->data.toJSON         ();}
-		auto insert_         (Path const &range, Data const &d, Path const &coroResultPath)                        -> bool           override {return this->data.insert         (range, d, coroResultPath);}
+		auto insert_         (Path const &range, Data const &d, Path const &coroResultPath)                        -> bool           override {return this->data.insert         (range, d,    coroResultPath);}
 		auto grab_           (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           override {return this->data.grab           (range, info, data, isTriviallyCopyable);}
 		auto grabBlock_      (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           override {return this->data.grabBlock      (range, info, data, isTriviallyCopyable);}
 		auto read_           (Path const &range, std::type_info const *info, void *data, bool isTriviallyCopyable) -> bool           override {return this->data.read           (range, info, data, isTriviallyCopyable);}
